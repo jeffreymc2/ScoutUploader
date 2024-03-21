b5c43757bac6ac8087e837325d114e83a21aa068
Jeff McDowell <jeffreymc@gmail.com>
2024-03-21 00:02:03 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -8,52 +8,58 @@ export async function middleware(request: NextRequest) {
 			headers: request.headers,
 		},
 	});
-
-	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-		{
-			cookies: {
-				get(name: string) {
-					return request.cookies.get(name)?.value;
-				},
-				set(name: string, value: string, options: CookieOptions) {
-					request.cookies.set({
-						name,
-						value,
-						...options,
-					});
-					response = NextResponse.next({
-						request: {
-							headers: request.headers,
-						},
-					});
-					response.cookies.set({
-						name,
-						value,
-						...options,
-					});
-				},
-				remove(name: string, options: CookieOptions) {
-					request.cookies.set({
-						name,
-						value: "",
-						...options,
-					});
-					response = NextResponse.next({
-						request: {
-							headers: request.headers,
-						},
-					});
-					response.cookies.set({
-						name,
-						value: "",
-						...options,
-					});
-				},
+	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+	const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+	
+	if (!supabaseUrl || !supabaseAnonKey) {
+	  console.error('Missing Supabase environment variables');
+	  // Handle the error or provide a fallback behavior
+	  return NextResponse.json({ error: 'Missing Supabase environment variables' }, { status: 500 });
+	}
+	
+	const supabase = createServerClient(supabaseUrl, supabaseAnonKey,{
+		cookies: {
+			get(name: string) {
+				return request.cookies.get(name)?.value;
 			},
-		}
-	);
+			set(name: string, value: string, options: CookieOptions) {
+				request.cookies.set({
+					name,
+					value,
+					...options,
+				});
+				response = NextResponse.next({
+					request: {
+						headers: request.headers,
+					},
+				});
+				response.cookies.set({
+					name,
+					value,
+					...options,
+				});
+			},
+			remove(name: string, options: CookieOptions) {
+				request.cookies.set({
+					name,
+					value: "",
+					...options,
+				});
+				response = NextResponse.next({
+					request: {
+						headers: request.headers,
+					},
+				});
+				response.cookies.set({
+					name,
+					value: "",
+					...options,
+				});
+			},
+		},
+	});
+	
+
 
 	const { data } = await supabase.auth.getSession();
 	const url = new URL(request.url);
