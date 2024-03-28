1976eb92633f1d6b72869775cb4ba842dc168604
Jeff McDowell <jeffreymc@gmail.com>
2024-03-27 23:51:53 -0400

    latest

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { KeyRound } from "lucide-react";
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { useSearchParams } from "next/navigation";
 import { FaMicrosoft } from "react-icons/fa6";
 import Image from "next/image";
-import PerfectGameLogo from "@/components/PGLogo"
+import PerfectGameLogo from "@/components/PGLogo";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 
 const AuthPageContent = () => {
   const params = useSearchParams();
@@ -25,40 +33,40 @@ const AuthPageContent = () => {
   };
 
   return (
-	<div className="flex items-center justify-center min-h-screen p-4">
-	<div className="w-full max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto rounded-lg shadow-lg border bg-white space-y-6 p-6 sm:p-8 md:p-10">
-	  <div className="text-center">
-		<PerfectGameLogo backgroundColor="#005cb9" />
-		<h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-blue-500">PG Scout | Profile Uploader</h1>
-		<p className="mt-2 text-sm sm:text-base md:text-lg text-gray-600">Sign In With Your PG Account</p>
-	  </div>
-	  <div className="flex flex-col gap-4">
-		<Button
-		  className="items-center justify-center gap-2 py-2 sm:py-3 text-sm sm:text-md md:text-lg"
-		  variant="outline"
-		  onClick={() => handleLoginWithOAuth("github")}
-		>
-		  <FaGithub className="text-lg md:text-xl" /> Github
-		</Button>
-		{/* <Button
-		  className="items-center justify-center gap-2 py-2 sm:py-3 text-sm sm:text-md md:text-lg"
-		  variant="outline"
-		  onClick={() => handleLoginWithOAuth("microsoft")}
-		>
-		  <FaMicrosoft className="text-lg md:text-xl" /> Microsoft
-		</Button> */}
-		<Button
-		  className="items-center justify-center gap-2 py-2 sm:py-3 text-sm sm:text-md md:text-lg"
-		  variant="outline"
-		  onClick={() => handleLoginWithOAuth("google")}
-		>
-		  <FcGoogle className="text-lg md:text-xl" /> Google
-		</Button>
-	  </div>
+   
+    <Card className="flex flex-col justify-center items-center mt-5 bg-white  border-none">
+		<div className="p-4">
+      <PerfectGameLogo backgroundColor="#005cb9" />
 	</div>
-  </div>
-  
-
+      
+      <CardContent className="w-full max-w-md px-4 py-8 mx-auto bg-white rounded-lg shadow-lg">
+	  <CardHeader className="text-center">
+        <CardTitle className="text-4xl font-bold font-pgFont text-blue-500">
+          PG Scout | Profile Uploader
+        </CardTitle>
+        <CardDescription className="text-lg text-gray-600">
+          Sign In With Your PG Account
+        </CardDescription>
+      </CardHeader>
+        <div className="flex flex-col gap-4">
+          <Button
+            className="flex items-center justify-center gap-2 py-3 text-base rounded-lg border border-gray-200 shadow-sm"
+            onClick={() => handleLoginWithOAuth("github")}
+          >
+            <FaGithub className="text-xl" /> Github
+          </Button>
+          <Button
+            className="flex items-center justify-center gap-2 py-3 text-base rounded-lg border border-gray-200 shadow-sm"
+            onClick={() => handleLoginWithOAuth("google")}
+          >
+            <FcGoogle className="text-xl" /> Google
+          </Button>
+        </div>
+      </CardContent>
+      <CardFooter className="text-center">
+        <p className="text-gray-500 p-5">©2024 Perfect Game Inc. All rights reserved.</p>
+      </CardFooter>
+    </Card>
   );
 };
 
