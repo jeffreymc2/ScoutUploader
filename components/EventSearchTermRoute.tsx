417645ce8fff3bda9617c4223f43ecf1154e4a89
Jeff McDowell <jeffreymc@gmail.com>
2024-04-03 11:52:52 -0400

    search

--- components/EventSearchTermRoute.tsx
+++ components/EventSearchTermRoute.tsx
@@ -109,6 +109,12 @@ export default function EventSearchRoute({ events }: EventSearchProps) {
           onChange={(e) => setSearchQuery(e.target.value)}
           placeholder="Search by event name"
           className="w-full pl-3 pr-10 py-2 border border-gray-300 rounded-md text-base"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              // Check if the pressed key is Enter
+              handleSearch();
+            }
+          }}
         />
         {searchQuery && (
           <button
