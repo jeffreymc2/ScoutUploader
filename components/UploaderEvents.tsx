fc3e1a81b45053c494654d40f0e7be95501a387b
Jeff McDowell <jeffreymc@gmail.com>
2024-04-02 23:34:21 -0400

    button fixes

--- components/UploaderEvents.tsx
+++ components/UploaderEvents.tsx
@@ -83,7 +83,7 @@ const UploaderEvents: React.FC<UploaderProps> = ({ EventID, EventName, TeamID })
         </div>
       </div>
       <Dashboard uppy={uppy} className="w-auto" hideUploadButton />
-      <Button id="upload-trigger" className="w-full" onClick={handleUpload}>
+      <Button id="upload-trigger" className="px-4 py-2 ml-2 w-full font-medium tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-primary/90" onClick={handleUpload}>
         Upload
       </Button>
     </div>
