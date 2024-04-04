362b07365291d0dc6ddaad873375841af9bb9b9a
Jeff McDowell <jeffreymc@gmail.com>
2024-04-03 22:34:07 -0400

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 // app/page.tsx
 
+import BackgroundImage from "@/components/BackgroundImage";
 import LandingPage from "@/components/LandingPage";
+import Image from "next/image";
 
 export default async function Page({ searchParams }: { searchParams: any }) {
   const playerId = searchParams.player_id ?? null;
+  const imageSrc = "https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/pg-bg.jpg"
   return (
     <>
+      <BackgroundImage
+      image={
+        <Image
+          src={imageSrc}
+          alt="Image Alt Text"
+          className="object-cover object-center"
+          fill
+        />
+      }
+    >
    <LandingPage />
    <div className="container mx-auto">
    </div>
+   </BackgroundImage>
     </>
   );
 }
