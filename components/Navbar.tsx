a6f46dd95af83f0bafe4e6ec5a59186bc1cd4c64
Jeff McDowell <jeffreymc@gmail.com>
2024-04-09 19:22:09 -0400

    navbar logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,11 +66,9 @@ export default async function Navbar() {
             {data.session && (
               <form action={logoutAction} className="flex">
                 <li>
-                  <Link href="/">
                   <Button className="relative -mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-800">
                     Logout
                   </Button>
-                  </Link>
                 </li>
               </form>
             )}
