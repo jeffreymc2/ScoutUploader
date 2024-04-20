bbda25793c8ff65de129b8449beafcb4a72edfae
Jeff McDowell <jeffreymc@gmail.com>
2024-04-19 22:48:42 -0400

    jyvjg

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -104,7 +104,7 @@ const UploadPage: React.FC<UploaderProps> = ({ playerid, FullName }) => {
         {isDragActive ? (
           <p>Drop the files here ...</p>
         ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag and drop some files here, or click to select files</p>
         )}
       </div>
       {isUploading && <p>Uploading...</p>}
