daef81b84e36c85f8c4e46683ac92a5d7f60ffec
Jeff McDowell <jeffreymc@gmail.com>
2024-05-08 00:39:22 -0400

    updated videp player

--- components/MediaComponents/HighlighVideoItem.tsx
+++ components/MediaComponents/HighlighVideoItem.tsx
@@ -1,18 +1,14 @@
+// @/components/HighlightVideoItem.tsx
 "use client";
-import { CSSProperties, forwardRef, HTMLAttributes } from "react";
+import { CSSProperties, forwardRef, HTMLAttributes, useContext } from "react";
 import { HighlightVideo } from "@/lib/types/types";
 import { Card } from "../ui/card";
 import Image from "next/image";
 import { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "../ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, } from "../ui/dialog";
 import Video from "next-video";
 import { RiDraggable } from "react-icons/ri";
+import { VideoPlayerContext } from "@/components/MediaComponents/VideoPlaylist";
 
 type Props = {
   video: HighlightVideo;
@@ -26,6 +22,7 @@ const HighlightVideoItem = forwardRef<HTMLDivElement, Props>(
     ref
   ) {
     const [isOpen, setIsOpen] = useState(false);
+    const { setCurrentVideo } = useContext(VideoPlayerContext);
 
     const handleDialogOpen = () => {
       setIsOpen(true);
@@ -61,6 +58,16 @@ const HighlightVideoItem = forwardRef<HTMLDivElement, Props>(
       ...style,
     };
 
+    const handleViewClick = () => {
+      setCurrentVideo({
+        id: parseInt(video.id),
+        url: video.url,
+        title: video.title,
+        start_time: video.start_time,
+        duration: video.duration,
+      });
+    };
+
     return (
       <>
         <div className="grid">
@@ -94,6 +101,7 @@ const HighlightVideoItem = forwardRef<HTMLDivElement, Props>(
                 </p>
               )}
             </div>
+            <button onClick={handleViewClick}>View</button>
             <RiDraggable className="text-3xl text-gray-400" />
           </Card>
         </div>
@@ -103,3 +111,109 @@ const HighlightVideoItem = forwardRef<HTMLDivElement, Props>(
 );
 
 export { HighlightVideoItem };
+
+// "use client";
+// import { CSSProperties, forwardRef, HTMLAttributes } from "react";
+// import { HighlightVideo } from "@/lib/types/types";
+// import { Card } from "../ui/card";
+// import Image from "next/image";
+// import { useState } from "react";
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogHeader,
+//   DialogTitle,
+// } from "../ui/dialog";
+// import Video from "next-video";
+// import { RiDraggable } from "react-icons/ri";
+
+// type Props = {
+//   video: HighlightVideo;
+//   isOpacityEnabled?: boolean;
+//   isDragging?: boolean;
+// } & HTMLAttributes<HTMLDivElement>;
+
+// const HighlightVideoItem = forwardRef<HTMLDivElement, Props>(
+//   function HighlightVideoItem(
+//     { video, isOpacityEnabled, isDragging, style, ...props },
+//     ref
+//   ) {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const handleDialogOpen = () => {
+//       setIsOpen(true);
+//     };
+
+//     const handleDialogClose = () => {
+//       setIsOpen(false);
+//     };
+
+//     const getTitleWithoutBrackets = (title: string) => {
+//       const bracketRegex = /\[(.*?)\]/;
+//       const match = title.match(bracketRegex);
+//       return match ? match[1] : title;
+//     };
+
+//     const getTitle = (title: string) => {
+//       const bracketRegex = /\[(.*?)\]/;
+//       const match = title.match(bracketRegex);
+//       return match ? title.replace(match[0], "") : title;
+//     };
+
+//     const filterDescription = (description: string | undefined) => {
+//       if (description) {
+//         return description.replace(/9999/g, "");
+//       }
+//       return description;
+//     };
+
+//     const styles: CSSProperties = {
+//       opacity: isOpacityEnabled ? "0.4" : "1",
+//       cursor: isDragging ? "grabbing" : "grab",
+//       transform: isDragging ? "scale(1.05)" : "scale(1)",
+//       ...style,
+//     };
+
+//     return (
+//       <>
+//         <div className="grid">
+//           <Card
+//             className="flex items-center gap-4 rounded-lg bg-white"
+//             onClick={handleDialogOpen}
+//             ref={ref}
+//             style={styles}
+//             {...props}
+//           >
+//             <Image
+//               src={
+//                 video.thumbnailUrl ||
+//                 "https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/638252106298352027-DKPlusHP%20(1).webp"
+//               }
+//               alt="Image"
+//               className="rounded-md object-cover"
+//               height="128"
+//               style={{ aspectRatio: "128/128", objectFit: "cover" }}
+//               width="128"
+//             />
+//             <div className="flex-1">
+//               {video.title && (
+//                 <p className="text-sm leading-4 font-bold text-gray-600 mt-2">
+//                   {getTitle(video.title)}
+//                 </p>
+//               )}
+//               {filterDescription(video.description) && (
+//                 <p className="text-xs mt-1">
+//                   {filterDescription(video.description)}
+//                 </p>
+//               )}
+//             </div>
+//             <RiDraggable className="text-3xl text-gray-400" />
+//           </Card>
+//         </div>
+//       </>
+//     );
+//   }
+// );
+
+// export { HighlightVideoItem };
