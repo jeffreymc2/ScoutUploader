88d4a7d8f1e70902c70e4e22767581b52397f989
Jeff McDowell <jeffreymc@gmail.com>
2024-05-07 23:48:29 -0400

    updated playlist functionality

--- components/MediaComponents/PlaylistBuilder.tsx
+++ components/MediaComponents/PlaylistBuilder.tsx
@@ -277,19 +277,184 @@
 // @/components/MediaComponents/PlaylistBuilder.tsx
 // @/components/MediaComponents/PlaylistBuilder.tsx
 // @/components/MediaComponents/PlaylistBuilder.tsx
+// "use client";
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   DndContext,
+//   DragOverlay,
+//   PointerSensor,
+//   useSensor,
+//   useSensors,
+//   DragStartEvent,
+//   DragEndEvent,
+//   TouchSensor,
+//   closestCenter,
+// } from "@dnd-kit/core";
+// import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+// import { HighlightVideo } from "@/lib/types/types";
+// import { SortableHighlightVideoItem } from "./SortableHighlightVideoItem";
+// import { HighlightVideoItem } from "@/components/MediaComponents/HighlighVideoItem";
+// import { supabaseBrowser } from "@/lib/supabase/browser";
+// import useUser from "@/app/hook/useUser";
+// import { DroppablePlaylist } from "./DroppablePlaylist";
+// import { RiDraggable } from "react-icons/ri";
+
+
+// interface PlaylistBuilderProps {
+//   initialVideos: HighlightVideo[];
+// }
+
+// export function PlaylistBuilder({ initialVideos }: PlaylistBuilderProps) {
+//   const [videos, setVideos] = useState<HighlightVideo[]>(initialVideos);
+//   const [playlist, setPlaylist] = useState<HighlightVideo[]>([]);
+//   const [activeVideoId, setActiveVideoId] = useState<string | null>(null);
+//   const [savedPlaylist, setSavedPlaylist] = useState<HighlightVideo[]>([]);
+
+//   const { data: user } = useUser();
+
+//   const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
+//   const handleDragStart = (event: DragStartEvent) => {
+//     setActiveVideoId(event.active.id as string);
+//   };
+
+//   const handleDragEnd = (event: DragEndEvent) => {
+//     const { active, over } = event;
+
+//     if (active.id !== over?.id) {
+//       const activeContainer = active.data.current?.sortable.containerId;
+//       const overContainer = over?.data.current?.sortable.containerId;
+
+//       if (activeContainer !== overContainer) {
+//         const activeIndex = activeContainer === "videos"
+//           ? videos.findIndex((video) => video.id === active.id)
+//           : playlist.findIndex((video) => video.id === active.id);
+
+//         if (activeIndex !== -1) {
+//           const [removed] = activeContainer === "videos"
+//             ? videos.splice(activeIndex, 1)
+//             : playlist.splice(activeIndex, 1);
+
+//           if (activeContainer === "videos") {
+//             setVideos([...videos]);
+//             setPlaylist([...playlist, removed]);
+//           } else {
+//             setPlaylist([...playlist]);
+//             setVideos([...videos, removed]);
+//           }
+//         }
+//       } else {
+//         const items = activeContainer === "videos" ? videos : playlist;
+//         const oldIndex = items.findIndex((video) => video.id === active.id);
+//         const newIndex = items.findIndex((video) => video.id === over?.id);
+
+//         if (oldIndex !== -1 && newIndex !== -1) {
+//           const reorderedItems = arrayMove(items, oldIndex, newIndex);
+//           activeContainer === "videos" ? setVideos(reorderedItems) : setPlaylist(reorderedItems);
+//         }
+//       }
+//     }
+
+//     setActiveVideoId(null);
+//   };
+
+
+//   const user2 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd";
+  
+//   useEffect(() => {
+//     const fetchSavedPlaylist = async () => {
+//       if (user) {
+//         const { data: playlistData, error } = await supabaseBrowser()
+//           .from("playlists")
+//           .select("playlist")
+//           .eq("user_id", user2)           
+//           // .eq("user_id", user.id)
+
+//           .single();
+
+//         if (error) {
+//           console.error("Error fetching saved playlist:", error);
+//         } else {
+//           const fetchedPlaylist = playlistData?.playlist as HighlightVideo[];
+//           setSavedPlaylist(fetchedPlaylist || []);
+//           setPlaylist(fetchedPlaylist || []);
+//         }
+//       }
+//     };
+
+//     fetchSavedPlaylist();
+//   }, [user]);
+
+
+//   const savePlaylist = async () => {
+//     if (user) {
+//       const { error } = await supabaseBrowser()
+//         .from("playlists")
+//         .upsert({ user_id: "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd", name: "My Playlist", playlist: playlist as any[] })
+//         .single();
+
+//       if (error) {
+//         console.error("Error saving playlist:", error);
+//       } else {
+//         console.log("Playlist saved successfully");
+//       }
+//     }
+//   };
+
+//   const activeVideo = activeVideoId ? videos.find((video) => video.id === activeVideoId) || playlist.find((video) => video.id === activeVideoId) : null;
+
+//   return (
+//     <DndContext
+//       sensors={sensors}
+//       collisionDetection={closestCenter}
+//       onDragStart={handleDragStart}
+//       onDragEnd={handleDragEnd}
+//     >
+//       <div className="grid grid-cols-2 gap-4">
+//       <div className="grid grid-cols-1 ml-4 ">
+//       <h2 className="my-4 font-pgFont text-2xl">Video Highlights</h2>
+//           <SortableContext items={videos} id="videos" strategy={verticalListSortingStrategy}>
+//             {videos.map((video) => (
+//               <SortableHighlightVideoItem key={video.id} video={video}/>
+//             ))}
+//           </SortableContext>
+//         </div>
+//         <div>
+//             <div className="grid grid-cols gap-4">
+//         <SortableContext items={playlist} id="playlist" strategy={verticalListSortingStrategy}>
+//           <DroppablePlaylist playlist={playlist} setPlaylist={setPlaylist} />
+//           {/* <SortableContext items={playlist} id="playlist" strategy={verticalListSortingStrategy}>
+//             {playlist.map((video) => (
+//               <SortableHighlightVideoItem key={video.id} video={video} />
+//             ))} */}
+//           <button onClick={savePlaylist}>Save Playlist</button>
+//           </SortableContext>
+//         </div>
+//         </div>
+        
+//       </div>
+//       <DragOverlay>
+//         {activeVideo ? <HighlightVideoItem video={activeVideo} /> : null}
+//       </DragOverlay>
+//     </DndContext>
+//   );
+// }
+
+// @/components/MediaComponents/PlaylistBuilder.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  DndContext,
-  DragOverlay,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  DragStartEvent,
-  DragEndEvent,
-  TouchSensor,
-  closestCenter,
+ DndContext,
+ DragOverlay,
+ PointerSensor,
+ useSensor,
+ useSensors,
+ DragStartEvent,
+ DragEndEvent,
+ TouchSensor,
+ closestCenter,
 } from "@dnd-kit/core";
 import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { HighlightVideo } from "@/lib/types/types";
@@ -298,115 +463,144 @@ import { HighlightVideoItem } from "@/components/MediaComponents/HighlighVideoIt
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import useUser from "@/app/hook/useUser";
 import { DroppablePlaylist } from "./DroppablePlaylist";
-import { RiDraggable } from "react-icons/ri";
-
 
 interface PlaylistBuilderProps {
-  initialVideos: HighlightVideo[];
+ initialVideos: HighlightVideo[];
 }
 
 export function PlaylistBuilder({ initialVideos }: PlaylistBuilderProps) {
-  const [videos, setVideos] = useState<HighlightVideo[]>(initialVideos);
-  const [playlist, setPlaylist] = useState<HighlightVideo[]>([]);
-  const [activeVideoId, setActiveVideoId] = useState<string | null>(null);
-  const { data: user } = useUser();
+ const [videos, setVideos] = useState<HighlightVideo[]>(initialVideos);
+ const [playlist, setPlaylist] = useState<HighlightVideo[]>([]);
+ const [savedPlaylist, setSavedPlaylist] = useState<HighlightVideo[]>([]);
+ const [activeVideoId, setActiveVideoId] = useState<string | null>(null);
+ const { data: user } = useUser();
 
-  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+ const user2 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd";
 
-  const handleDragStart = (event: DragStartEvent) => {
-    setActiveVideoId(event.active.id as string);
-  };
+ useEffect(() => {
+   const fetchSavedPlaylist = async () => {
+     if (user) {
+       const { data: playlistData, error } = await supabaseBrowser()
+         .from("playlists")
+         .select("playlist")
+         .eq("user_id", user.id)
+         .single();
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
+       if (error) {
+         console.error("Error fetching saved playlist:", error);
+       } else {
+         const fetchedPlaylist = playlistData?.playlist as HighlightVideo[];
+         setSavedPlaylist(fetchedPlaylist || []);
+         setPlaylist(fetchedPlaylist || []);
+         setVideos(initialVideos.filter((video) => !fetchedPlaylist?.some((savedVideo) => savedVideo.id === video.id)));
+       }
+     }
+   };
 
-    if (active.id !== over?.id) {
-      const activeContainer = active.data.current?.sortable.containerId;
-      const overContainer = over?.data.current?.sortable.containerId;
+   fetchSavedPlaylist();
+ }, [user, initialVideos]);
 
-      if (activeContainer !== overContainer) {
-        const activeIndex = activeContainer === "videos"
-          ? videos.findIndex((video) => video.id === active.id)
-          : playlist.findIndex((video) => video.id === active.id);
+ const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
 
-        if (activeIndex !== -1) {
-          const [removed] = activeContainer === "videos"
-            ? videos.splice(activeIndex, 1)
-            : playlist.splice(activeIndex, 1);
+ const handleDragStart = (event: DragStartEvent) => {
+   setActiveVideoId(event.active.id as string);
+ };
 
-          if (activeContainer === "videos") {
-            setVideos([...videos]);
-            setPlaylist([...playlist, removed]);
-          } else {
-            setPlaylist([...playlist]);
-            setVideos([...videos, removed]);
-          }
-        }
-      } else {
-        const items = activeContainer === "videos" ? videos : playlist;
-        const oldIndex = items.findIndex((video) => video.id === active.id);
-        const newIndex = items.findIndex((video) => video.id === over?.id);
+ const handleDragEnd = (event: DragEndEvent) => {
+   const { active, over } = event;
 
-        if (oldIndex !== -1 && newIndex !== -1) {
-          const reorderedItems = arrayMove(items, oldIndex, newIndex);
-          activeContainer === "videos" ? setVideos(reorderedItems) : setPlaylist(reorderedItems);
-        }
-      }
-    }
+   if (active.id !== over?.id) {
+     const activeContainer = active.data.current?.sortable.containerId;
+     const overContainer = over?.data.current?.sortable.containerId;
 
-    setActiveVideoId(null);
-  };
+     if (activeContainer !== overContainer) {
+       const activeIndex =
+         activeContainer === "videos"
+           ? videos.findIndex((video) => video.id === active.id)
+           : playlist.findIndex((video) => video.id === active.id);
 
-  const savePlaylist = async () => {
-    if (user) {
-      const { error } = await supabaseBrowser()
-        .from("playlists")
-        .upsert({ user_id: "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd", name: "My Playlist", playlist: playlist as any[] })
-        .single();
+       if (activeIndex !== -1) {
+         const [removed] =
+           activeContainer === "videos"
+             ? videos.splice(activeIndex, 1)
+             : playlist.splice(activeIndex, 1);
 
-      if (error) {
-        console.error("Error saving playlist:", error);
-      } else {
-        console.log("Playlist saved successfully");
-      }
-    }
-  };
+         if (activeContainer === "videos") {
+           setVideos([...videos]);
+           setPlaylist([...playlist, removed]);
+           setSavedPlaylist([...savedPlaylist, removed]);
+         } else {
+           setPlaylist([...playlist]);
+           setVideos([...videos, removed]);
+           setSavedPlaylist(savedPlaylist.filter((video) => video.id !== active.id));
+         }
+       }
+     } else {
+       const items = activeContainer === "videos" ? videos : playlist;
+       const oldIndex = items.findIndex((video) => video.id === active.id);
+       const newIndex = items.findIndex((video) => video.id === over?.id);
 
-  const activeVideo = activeVideoId ? videos.find((video) => video.id === activeVideoId) || playlist.find((video) => video.id === activeVideoId) : null;
+       if (oldIndex !== -1 && newIndex !== -1) {
+         const reorderedItems = arrayMove(items, oldIndex, newIndex);
+         activeContainer === "videos"
+           ? setVideos(reorderedItems)
+           : setPlaylist(reorderedItems);
+         activeContainer === "playlist" && setSavedPlaylist(reorderedItems);
+       }
+     }
+   }
 
-  return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd}
-    >
-      <div className="grid grid-cols-2 gap-4">
-      <div className="grid grid-cols-1 ml-4 ">
-      <h2 className="my-4 font-pgFont text-2xl">Video Highlights</h2>
-          <SortableContext items={videos} id="videos" strategy={verticalListSortingStrategy}>
-            {videos.map((video) => (
-              <SortableHighlightVideoItem key={video.id} video={video}/>
-            ))}
-          </SortableContext>
-        </div>
-        <div>
-            <div className="grid grid-cols gap-4">
-        <SortableContext items={playlist} id="playlist" strategy={verticalListSortingStrategy}>
-          <DroppablePlaylist playlist={playlist} setPlaylist={setPlaylist} />
-          {/* <SortableContext items={playlist} id="playlist" strategy={verticalListSortingStrategy}>
-            {playlist.map((video) => (
-              <SortableHighlightVideoItem key={video.id} video={video} />
-            ))} */}
-          <button onClick={savePlaylist}>Save Playlist</button>
-          </SortableContext>
-        </div>
-        </div>
-        
-      </div>
-      <DragOverlay>
-        {activeVideo ? <HighlightVideoItem video={activeVideo} /> : null}
-      </DragOverlay>
-    </DndContext>
-  );
+   setActiveVideoId(null);
+ };
+
+//  const user3 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd";
+ const savePlaylist = async () => {
+   if (user) {
+     const { error } = await supabaseBrowser()
+       .from("playlists")
+       .upsert({ user_id: user.id, name: "My Playlist", playlist: playlist as any[] })
+       .single();
+
+     if (error) {
+       console.error("Error saving playlist:", error);
+     } else {
+       console.log("Playlist saved successfully");
+     }
+   }
+ };
+
+ const activeVideo =
+   activeVideoId && (videos.find((video) => video.id === activeVideoId) ||
+   playlist.find((video) => video.id === activeVideoId));
+
+ return (
+   <DndContext
+     sensors={sensors}
+     collisionDetection={closestCenter}
+     onDragStart={handleDragStart}
+     onDragEnd={handleDragEnd}
+   >
+     <div className="grid grid-cols-2 gap-4 mb-4">
+     <div className="mb-4 w-full">
+        <h2 className="my-4 font-pgFont text-2xl">Highlights</h2>
+         <SortableContext items={videos} id="videos" strategy={verticalListSortingStrategy}>
+           {videos.map((video) => (
+             <SortableHighlightVideoItem key={video.id} video={video} />
+           ))}
+         </SortableContext>
+       </div>
+       <div>
+         {savedPlaylist.length > 0 ? (
+           <DroppablePlaylist playlist={playlist} setPlaylist={setPlaylist} />
+         ) : (
+           <p>No saved playlist found.</p>
+         )}
+         <button onClick={savePlaylist}>Save Playlist</button>
+       </div>
+     </div>
+     <DragOverlay>
+       {activeVideo ? <HighlightVideoItem video={activeVideo} /> : null}
+     </DragOverlay>
+   </DndContext>
+ );
 }
--- components/MediaComponents/VideoPlaylist.tsx
+++ components/MediaComponents/VideoPlaylist.tsx
@@ -1,16 +1,17 @@
+// @/components/VideoPlayer.tsx
 "use client";
-
 import React, { useEffect, useState } from "react";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import useUser from "@/app/hook/useUser";
-// import Video from "next-video";
 import ReactPlayer from "react-player";
 
 interface Video {
-    id: number;
-    url: string;
-    title: string;
-    // Add other properties as needed
+  id: number;
+  url: string;
+  title: string;
+  start_time: number;
+  duration: number;
+  // Add other properties as needed
 }
 
 const VideoPlayer: React.FC = () => {
@@ -18,13 +19,15 @@ const VideoPlayer: React.FC = () => {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const { data: user } = useUser();
 
+  const user2 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd"
+
   useEffect(() => {
     const fetchPlaylist = async () => {
       if (user) {
         const { data: playlistData, error } = await supabaseBrowser()
           .from("playlists")
           .select("playlist")
-          .eq("user_id", "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd")
+          .eq("user_id", user.id)
           .single();
 
         if (error) {
@@ -50,7 +53,20 @@ const VideoPlayer: React.FC = () => {
 
   return (
     <div>
-      <ReactPlayer url={currentVideo.url} controls onEnded={handleVideoEnded} />
+      <ReactPlayer
+        url={currentVideo.url}
+        controls
+        playing
+        onEnded={handleVideoEnded}
+        config={{
+          file: {
+            attributes: {
+              start: currentVideo.start_time,
+              end: currentVideo.start_time + currentVideo.duration,
+            },
+          },
+        }}
+      />
       <h3>{currentVideo.title}</h3>
       {/* Display other video information as needed */}
     </div>
