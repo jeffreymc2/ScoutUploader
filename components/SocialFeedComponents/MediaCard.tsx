bb60b3e29747edf35632a797e9e47d66367ff899
Jeff McDowell <jeffreymc@gmail.com>
2024-05-10 15:08:17 -0400

    social

--- components/SocialFeedComponents/MediaCard.tsx
+++ components/SocialFeedComponents/MediaCard.tsx
@@ -5,9 +5,10 @@ import { CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button";
 import { MessageSquareIcon, ShareIcon, ThumbsUpIcon } from "lucide-react";
 import { useState } from "react";
-import Video from "next-video";
 import { Post, Playlist, HighlightVideo } from "@/lib/types/types";
 import Image from "next/image";
+import React from "react";
+import ReactPlayer from "react-player";
 
 interface MediaCardProps {
   media: Post | Playlist | HighlightVideo;
@@ -15,8 +16,16 @@ interface MediaCardProps {
 
 export default function MediaCard({ media }: MediaCardProps) {
   const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 
   const handleLike = () => {
+    if (!isLiked) {
+      setLikeCount((prevCount) => prevCount + 1);
+    } else {
+      setLikeCount((prevCount) => Math.max(prevCount - 1, 0));
+    }
     setIsLiked(!isLiked);
     console.log(`Liked media with ID: ${media.id}`);
   };
@@ -39,8 +48,27 @@ export default function MediaCard({ media }: MediaCardProps) {
     return match ? match[1] : title;
   };
 
+  const playerRef = React.useRef<ReactPlayer | null>(null);
 
-  // Get the title of the highlight without the brackets
+  const onReady = React.useCallback(() => {
+    if (playerRef.current && isHighlightVideo(media)) {
+      playerRef.current.seekTo(media.start_time as number, "seconds");
+    }
+  }, [playerRef.current, media]);
+
+  const handleProgress = (state: { playedSeconds: number }) => {
+    setCurrentTime(state.playedSeconds);
+
+    if (isHighlightVideo(media)) {
+      if (
+        state.playedSeconds >=
+        (media.start_time as number) + (media.duration as number)
+      ) {
+        setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % 1);
+        setCurrentTime(0);
+      }
+    }
+  };
 
   const getTitle = (title: string) => {
     const bracketRegex = /\[(.*?)\]/;
@@ -48,17 +76,17 @@ export default function MediaCard({ media }: MediaCardProps) {
     return match ? title.replace(match[0], "") : title;
   };
 
-  // Render the badge with the title of the highlight
   const renderOverlayBadge = (title: string) => {
     const titleWithoutBrackets = getTitleWithoutBrackets(title);
     return (
       <div className="absolute top-2 left-2">
-        <Badge variant="secondary"><Image className="mr-2" src={"https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_icon_inverse.png"} width={55} height={10} alt={""}></Image> </Badge>
+        <Badge variant="secondary">
+          <Image className="mr-2" src={"https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_icon_inverse.png"} width={55} height={10} alt={""} />
+        </Badge>
       </div>
     );
   };
 
-  // Filter out the 9999 from the description
   const filterDescription = (description: string | undefined) => {
     if (description) {
       return description.replace(/9999/g, "");
@@ -66,40 +94,36 @@ export default function MediaCard({ media }: MediaCardProps) {
     return description;
   };
 
-
   return (
     <>
-    <div className="flex items-center my-4 mx-auto">
-                <Image
-                  src="https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_horizontal_primary%20(3).png"
-                  alt="Image"
-                  height={75}
-                  width={250}
-                  className="mr-4"
-                />
-                <h2 className="font-pgFont text-2xl">Highlights</h2>
-              </div>
-    <Card className="w-full mb-4 rounded-lg shadow-lg">
-      {/* <CardHeader>
-        <div className="flex justify-between items-start">
-          <Badge variant="secondary">
-            {isHighlightVideo(media) ? media.title : "Unknown"}
-          </Badge>
-        </div>
-      </CardHeader>
-      */}
-      <CardContent className="mt-4">
-        {isHighlightVideo(media) && (
-          <Video
-          className="rounded-lg absolute top-0 left-0"
-          src={media.url}
-          autoPlay={false}
-          preload="auto"
-          startTime={media.start_time}
-          placeholder={media.thumbnailUrl || "/placeholder.png"}
+      <div className="flex items-center my-4 mx-auto">
+        <Image
+          src="https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_horizontal_primary%20(3).png"
+          alt="Image"
+          height={75}
+          width={250}
+          className="mr-4"
         />
-        )}
-               {media.title && (
+        <h2 className="font-pgFont text-2xl">Highlights</h2>
+      </div>
+      <Card className="w-full mb-4 rounded-lg shadow-lg">
+        <CardContent className="mt-4">
+          {isHighlightVideo(media) && (
+            <ReactPlayer
+              url={media.url as string}
+              controls={true}
+              playing={false}
+              muted={true}
+              volume={0}
+              width={"100%"}
+              height={"100%"}
+              onProgress={handleProgress}
+              onReady={onReady}
+              className="w-full h-full rounded-lg object-cover"
+              ref={playerRef}
+            />
+          )}
+          {media.title && (
             <p className="text-md leading-4 font-bold text-gray-600 mt-2">
               {getTitle(media.title)}
             </p>
@@ -109,22 +133,22 @@ export default function MediaCard({ media }: MediaCardProps) {
               {filterDescription(media.description)}
             </p>
           )}
-      </CardContent>
-      <CardFooter className="flex space-x-4">
-        <Button variant="ghost" onClick={handleLike}>
-          <ThumbsUpIcon className={`mr-1 ${isLiked ? "text-blue-500" : ""}`} />
-          Like
-        </Button>
-        <Button variant="ghost" onClick={handleComment}>
-          <MessageSquareIcon className="mr-1" />
-          Comment
-        </Button>
-        <Button variant="ghost" onClick={handleShare}>
-          <ShareIcon className="mr-1" />
-          Share
-        </Button>
-      </CardFooter>
-    </Card>
+        </CardContent>
+        <CardFooter className="flex space-x-4">
+          <Button variant="ghost" onClick={handleLike}>
+            <ThumbsUpIcon className={`mr-1 ${isLiked ? "text-blue-500" : ""}`} />
+            {likeCount > 0 ? `${likeCount} Likes` : "Like"}
+          </Button>
+          <Button variant="ghost" onClick={handleComment}>
+            <MessageSquareIcon className="mr-1" />
+            Comment
+          </Button>
+          <Button variant="ghost" onClick={handleShare}>
+            <ShareIcon className="mr-1" />
+            Share
+          </Button>
+        </CardFooter>
+      </Card>
     </>
   );
 }
