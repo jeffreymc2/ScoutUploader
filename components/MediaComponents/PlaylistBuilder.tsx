3b1860d92cac788c8f0d246ce4d664cbdf7fb718
Jeff McDowell <jeffreymc@gmail.com>
2024-05-18 21:57:32 -0400

    starttime updated

--- components/MediaComponents/PlaylistBuilder.tsx
+++ components/MediaComponents/PlaylistBuilder.tsx
@@ -1,260 +1,3 @@
-// This component is used to build a playlist of highlight videos. It allows the user to drag and drop videos between two lists, one for videos that have not been added to the playlist and one for videos that have been added to the playlist. The user can save the playlist to the database if they are logged in.
-// The component uses the DndContext component from the @dnd-kit/core library to handle drag and drop functionality. It also uses the SortableContext and SortableHighlightVideoItem components from the @dnd-kit/sortable library to create the sortable lists of videos.
-// The component fetches the saved playlist from the database when it mounts and updates the playlist state accordingly. It also updates the saved playlist state when the user adds or removes videos from the playlist.
-// The component uses the useUser hook to get the current user from the auth context. It also uses the useRouter hook from the next/navigation library to refresh the page after saving the playlist.
-
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import {
-//   DndContext,
-//   DragOverlay,
-//   PointerSensor,
-//   useSensor,
-//   useSensors,
-//   DragStartEvent,
-//   DragEndEvent,
-//   TouchSensor,
-//   closestCenter,
-//   MeasuringStrategy,
-// } from "@dnd-kit/core";
-// import {
-//   arrayMove,
-//   SortableContext,
-//   verticalListSortingStrategy,
-// } from "@dnd-kit/sortable";
-// import { HighlightVideo } from "@/lib/types/types";
-// import { SortableHighlightVideoItem } from "./SortableHighlightVideoItem";
-// import { HighlightVideoItem } from "@/components/MediaComponents/HighlighVideoItem";
-// import { supabaseBrowser } from "@/lib/supabase/browser";
-// import useUser from "@/app/hook/useUser";
-// import { DroppablePlaylist } from "./DroppablePlaylist";
-// import { Button } from "../ui/button";
-// import { toast } from "sonner";
-// import { useRouter } from "next/navigation"; // Import useRouter
-// import { Card } from "../ui/card";
-// import { strict } from "assert";
-// import Image from "next/image";
-
-// interface PlaylistBuilderProps {
-//   initialVideos: HighlightVideo[];
-//   playerId: string;
-// }
-
-// export function PlaylistBuilder({
-//   initialVideos,
-//   playerId,
-// }: PlaylistBuilderProps) {
-//   const [videos, setVideos] = useState<HighlightVideo[]>(initialVideos);
-//   const [playlist, setPlaylist] = useState<HighlightVideo[]>([]);
-//   const [savedPlaylist, setSavedPlaylist] = useState<HighlightVideo[]>([]);
-//   const [activeVideoId, setActiveVideoId] = useState<string | null>(null);
-//   const { data: user } = useUser();
-//   const router = useRouter(); // Get the router instance
-
-//   const user2 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd";
-
-//   useEffect(() => {
-//     const fetchSavedPlaylist = async () => {
-//       if (user) {
-//         const { data: playlistData, error } = await supabaseBrowser()
-//           .from("playlists")
-//           .select("playlist")
-//           .eq("user_id", user.id)
-//           .eq("player_id", playerId)
-//           .maybeSingle();
-
-//         if (error) {
-//           console.error("Error fetching saved playlist:", error);
-//         } else {
-//           const fetchedPlaylist = playlistData?.playlist as HighlightVideo[];
-//           setSavedPlaylist(fetchedPlaylist || []);
-//           setPlaylist(fetchedPlaylist || []);
-//           setVideos(
-//             initialVideos.filter(
-//               (video) =>
-//                 !fetchedPlaylist?.some(
-//                   (savedVideo) => savedVideo.id === video.id
-//                 )
-//             )
-//           );
-//         }
-//       }
-//     };
-
-//     fetchSavedPlaylist();
-//   }, [user, initialVideos, playerId]);
-
-//   const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
-
-//   const handleDragStart = (event: DragStartEvent) => {
-//     setActiveVideoId(event.active.id as string);
-//   };
-
-//   const handleDragEnd = (event: DragEndEvent) => {
-//     const { active, over } = event;
-
-//     if (active.id !== over?.id) {
-//       const activeContainer = active.data.current?.sortable.containerId;
-//       const overContainer = over?.data.current?.sortable.containerId;
-
-//       if (activeContainer !== overContainer) {
-//         const activeIndex =
-//           activeContainer === "videos"
-//             ? videos.findIndex((video) => video.id === active.id)
-//             : playlist.findIndex((video) => video.id === active.id);
-
-//         if (activeIndex !== -1) {
-//           const [removed] =
-//             activeContainer === "videos"
-//               ? videos.splice(activeIndex, 1)
-//               : playlist.splice(activeIndex, 1);
-
-//           if (activeContainer === "videos") {
-//             setVideos([...videos]);
-//             setPlaylist([...playlist, removed]);
-//             setSavedPlaylist([...savedPlaylist, removed]);
-//           } else {
-//             setPlaylist([...playlist]);
-//             setVideos([...videos, removed]);
-//             setSavedPlaylist(
-//               savedPlaylist.filter((video) => video.id !== active.id)
-//             );
-//           }
-//         }
-//       } else {
-//         const items = activeContainer === "videos" ? videos : playlist;
-//         const oldIndex = items.findIndex((video) => video.id === active.id);
-//         const newIndex = items.findIndex((video) => video.id === over?.id);
-
-//         if (oldIndex !== 1 && newIndex !== 1) {
-//           const reorderedItems = arrayMove(items, oldIndex, newIndex);
-//           activeContainer === "videos"
-//             ? setVideos(reorderedItems)
-//             : setPlaylist(reorderedItems);
-//           activeContainer === "playlist" && setSavedPlaylist(reorderedItems);
-//         }
-//       }
-//     }
-
-//     setActiveVideoId(null);
-//   };
-
-//   const user3 = "3faf9652-84d8-4b76-8b44-8e1f3b7ff7fd";
-//   const savePlaylist = async () => {
-//     if (user) {
-//       const { data: existingPlaylist, error: fetchError } =
-//         await supabaseBrowser()
-//           .from("playlists")
-//           .select("id")
-//           .eq("user_id", user.id)
-//           .eq("player_id", playerId)
-//           .maybeSingle();
-
-//       if (fetchError) {
-//         console.error("Error fetching existing playlist:", fetchError);
-//         return;
-//       }
-
-//       if (existingPlaylist) {
-//         // Update the existing playlist
-//         const { error: updateError } = await supabaseBrowser()
-//           .from("playlists")
-//           .update({
-//             playlist: playlist as any[],
-//             updated_at: new Date().toISOString(),
-//           })
-//           .eq("id", existingPlaylist.id);
-
-//         if (updateError) {
-//           console.error("Error updating playlist:", updateError);
-//           toast.error("Failed to update playlist");
-//         } else {
-//           console.log("Playlist updated successfully");
-//           toast.success("Playlist updated successfully");
-//         }
-//       } else {
-//         // Create a new playlist
-//         const { error: insertError } = await supabaseBrowser()
-//           .from("playlists")
-//           .insert({
-//             user_id: user.id,
-//             player_id: playerId,
-//             name: "My Playlist",
-//             playlist: playlist as any[],
-//           })
-//           .single();
-
-//         if (insertError) {
-//           console.error("Error creating playlist:", insertError);
-//           toast.error("Failed to create playlist");
-//         } else {
-//           console.log("Playlist created successfully");
-//           toast.success("Playlist created successfully");
-//         }
-//       }
-
-//       router.refresh();
-//     } else {
-//       toast.error("You must be logged in to save a playlist");
-//     }
-//   };
-
-//   const activeVideo =
-//     activeVideoId &&
-//     (videos.find((video) => video.id === activeVideoId) ||
-//       playlist.find((video) => video.id === activeVideoId));
-
-//   return (
-//     <>
-//       <DndContext
-//         sensors={sensors}
-//         collisionDetection={closestCenter}
-//         onDragStart={handleDragStart}
-//         onDragEnd={handleDragEnd}
-//       >
-//         <Card>
-//           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4 ml-4 mr-4">
-//             <div className="mb-4 w-full">
-//               <div className="flex items-center my-4">
-//                 <Image
-//                   src="https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_horizontal_primary%20(3).png"
-//                   alt="Image"
-//                   height={75}
-//                   width={250}
-//                   className="mr-4"
-//                 />
-//                 <h2 className="font-pgFont text-2xl">Highlights</h2>
-//               </div>
-//               <SortableContext
-//                 items={videos}
-//                 id="videos"
-//                 strategy={verticalListSortingStrategy}
-//               >
-//                 {videos.map((video) => (
-//                   <SortableHighlightVideoItem key={video.id} video={video} />
-//                 ))}
-//               </SortableContext>
-//             </div>
-//             <div>
-//               <DroppablePlaylist
-//                 playlist={playlist}
-//                 setPlaylist={setPlaylist}
-//               />
-//               <Button onClick={savePlaylist} disabled={!user}>
-//                 Save Playlist
-//               </Button>{" "}
-//             </div>
-//           </div>
-//         </Card>
-//         <DragOverlay>
-//           {activeVideo ? <HighlightVideoItem video={activeVideo} /> : null}
-//         </DragOverlay>
-//       </DndContext>
-//     </>
-//   );
-// }
-
 
 "use client";
 
@@ -268,14 +11,9 @@ import {
   DragStartEvent,
   DragEndEvent,
   TouchSensor,
-  closestCenter,
   closestCorners,
 } from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
+import { arrayMove } from "@dnd-kit/sortable";
 import { HighlightVideo } from "@/lib/types/types";
 import { HighlightVideoItem } from "@/components/MediaComponents/HighlighVideoItem";
 import { supabaseBrowser } from "@/lib/supabase/browser";
@@ -283,8 +21,7 @@ import useUser from "@/app/hook/useUser";
 import { DroppablePlaylist } from "./DroppablePlaylist";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation"; 
-import { Card } from "../ui/card";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 interface PlaylistBuilderProps {
@@ -292,16 +29,13 @@ interface PlaylistBuilderProps {
   playerId: string;
 }
 
-export function PlaylistBuilder({
-  initialVideos,
-  playerId,
-}: PlaylistBuilderProps) {
+export function PlaylistBuilder({ initialVideos, playerId }: PlaylistBuilderProps) {
   const [videos, setVideos] = useState<HighlightVideo[]>(initialVideos);
   const [playlist, setPlaylist] = useState<HighlightVideo[]>([]);
   const [savedPlaylist, setSavedPlaylist] = useState<HighlightVideo[]>([]);
   const [activeVideoId, setActiveVideoId] = useState<string | null>(null);
   const { data: user } = useUser();
-  const router = useRouter(); 
+  const router = useRouter();
 
   useEffect(() => {
     const fetchSavedPlaylist = async () => {
@@ -321,10 +55,7 @@ export function PlaylistBuilder({
           setPlaylist(fetchedPlaylist?.filter(Boolean) || []);
           setVideos(
             initialVideos.filter(
-              (video) =>
-                !fetchedPlaylist?.some(
-                  (savedVideo) => savedVideo.id === video.id
-                )
+              (video) => !fetchedPlaylist?.some((savedVideo) => savedVideo.id === video.id)
             )
           );
         }
@@ -371,9 +102,7 @@ export function PlaylistBuilder({
           } else {
             setPlaylist([...playlist]);
             setVideos([removed, ...videos]);
-            setSavedPlaylist(
-              savedPlaylist.filter((video) => video.id !== active.id)
-            );
+            setSavedPlaylist(savedPlaylist.filter((video) => video.id !== active.id));
           }
         }
       } else {
@@ -407,13 +136,12 @@ export function PlaylistBuilder({
 
   const savePlaylist = async () => {
     if (user) {
-      const { data: existingPlaylist, error: fetchError } =
-        await supabaseBrowser()
-          .from("playlists")
-          .select("id")
-          .eq("user_id", user.id)
-          .eq("player_id", playerId)
-          .maybeSingle();
+      const { data: existingPlaylist, error: fetchError } = await supabaseBrowser()
+        .from("playlists")
+        .select("id")
+        .eq("user_id", user.id)
+        .eq("player_id", playerId)
+        .maybeSingle();
 
       if (fetchError) {
         console.error("Error fetching existing playlist:", fetchError);
@@ -435,6 +163,7 @@ export function PlaylistBuilder({
         } else {
           console.log("Playlist updated successfully");
           toast.success("Playlist updated successfully");
+          router.refresh();
         }
       } else {
         const { error: insertError } = await supabaseBrowser()
@@ -453,10 +182,9 @@ export function PlaylistBuilder({
         } else {
           console.log("Playlist created successfully");
           toast.success("Playlist created successfully");
+          router.refresh();
         }
       }
-
-      router.refresh();
     } else {
       toast.error("You must be logged in to save a playlist");
     }
@@ -475,38 +203,38 @@ export function PlaylistBuilder({
         onDragStart={handleDragStart}
         onDragEnd={handleDragEnd}
       >
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4 ml-4 mr-4">
-            <div className="mb-4 w-full">
-              <div className="flex items-center my-4">
-                <Image
-                  src="https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_horizontal_primary%20(3).png"
-                  alt="Image"
-                  height={150}
-                  width={200}
-                  className="mr-4"
-                />
-                <h2 className="font-pgFont text-2xl">Highlights</h2>
-              </div>
-              {videos.map((video) => (
-                <HighlightVideoItem
-                  key={video.id}
-                  video={video}
-                  isInPlaylist={false}
-                  onAddRemove={() => handleAddRemove(video)}
-                />
-              ))}
-            </div>
-            <div>
-              <DroppablePlaylist
-                playlist={playlist}
-                setPlaylist={setPlaylist}
-                onAddRemove={handleAddRemove}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4 ml-4 mr-4">
+          <div className="mb-4 w-full">
+            <div className="flex items-center my-4">
+              <Image
+                src="https://avkhdvyjcweghosyfiiw.supabase.co/storage/v1/object/public/misc/dkPlus_horizontal_primary%20(3).png"
+                alt="Image"
+                height={150}
+                width={200}
+                className="mr-4"
               />
-              <Button onClick={savePlaylist} disabled={!user}>
-                Save Playlist
-              </Button>{" "}
+              <h2 className="font-pgFont text-2xl">Highlights</h2>
             </div>
+            {videos.map((video) => (
+              <HighlightVideoItem
+                key={video.id}
+                video={video}
+                isInPlaylist={false}
+                onAddRemove={() => handleAddRemove(video)}
+              />
+            ))}
           </div>
+          <div>
+            <DroppablePlaylist
+              playlist={playlist}
+              setPlaylist={setPlaylist}
+              onAddRemove={handleAddRemove}
+            />
+            <Button onClick={savePlaylist} disabled={!user}>
+              Save Playlist
+            </Button>{" "}
+          </div>
+        </div>
         {/* <DragOverlay>
           {activeVideo ? <HighlightVideoItem video={activeVideo} isInPlaylist={true} onAddRemove={() => handleAddRemove(activeVideo)} /> : null}
         </DragOverlay> */}
