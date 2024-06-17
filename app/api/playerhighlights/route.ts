2991c161f5d0cef0f23e499a9fa4df31bd12947b
Jeff McDowell <jeffreymc@gmail.com>
2024-06-17 10:41:05 -0400

    fetch cors

--- app/api/playerhighlights/route.ts
+++ app/api/playerhighlights/route.ts
@@ -151,7 +151,7 @@ export async function GET(request: NextRequest) {
         status: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,OPTIONS",
+          "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type,Origin,Access-Control-Request-Headers,Access-Control-Request-Method",
         },
       }
@@ -172,7 +172,7 @@ export async function OPTIONS() {
       status: 204,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,OPTIONS",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type,Origin,Access-Control-Request-Headers,Access-Control-Request-Method",
       },
     }
