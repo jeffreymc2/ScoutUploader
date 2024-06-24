542a9a0c762bd6b121c97086b774de1aaa76660d
Jeff McDowell <jeffreymc@gmail.com>
2024-06-24 12:58:07 -0400

    media form

--- components/UtilityComponents/DeletePost.tsx
+++ components/UtilityComponents/DeletePost.tsx
@@ -1,14 +1,12 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "../ui/button";
 import useUser from "@/app/hook/useUser";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import {getUserData} from "@/lib/useUser";
-
-import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { MediaFile } from "@/lib/types/types";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -20,6 +18,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 interface DeletePostProps {
   postId: string;
@@ -27,25 +26,12 @@ interface DeletePostProps {
   filePath: string;
 }
 
-const DeletePost: React.FC<DeletePostProps> = async ({ postId, post_by, filePath }) => {
-  const supabase = supabaseBrowser();
+const DeletePost: React.FC<DeletePostProps> = ({ postId, post_by, filePath }) => {
+  const { data: user, isFetching } = useUser();
   const router = useRouter();
-
-  const getS3KeyFromCloudFrontURL = (url: string) => {
-    const cloudFrontDomain = process.env.NEXT_PUBLIC_CLOUDFRONT_DOMAIN; // Example: "https://d123.cloudfront.net/"
-    if (cloudFrontDomain && url.startsWith(cloudFrontDomain)) {
-      const newFilepath = url.replace(cloudFrontDomain, "");
-      return newFilepath;
-    }
-    throw new Error("Invalid CloudFront URL or Domain");
-  };
+  const supabase = supabaseBrowser();
 
   const handleDelete = async () => {
-    if (!filePath || filePath.includes("undefined")) {
-      toast.error("Invalid file path");
-      return;
-    }
-
     toast.info("Deleting post...");
     try {
       // Delete the post from the 'posts' table in Supabase
@@ -58,9 +44,6 @@ const DeletePost: React.FC<DeletePostProps> = async ({ postId, post_by, filePath
         throw new Error(deletePostError.message);
       }
 
-      // Extract the S3 key from the CloudFront URL
-      const s3Key = getS3KeyFromCloudFrontURL(filePath);
-
       // Delete the corresponding file from S3
       const s3Client = new S3Client({
         region: process.env.NEXT_PUBLIC_AWS_REGION,
@@ -72,7 +55,7 @@ const DeletePost: React.FC<DeletePostProps> = async ({ postId, post_by, filePath
 
       const deleteFileCommand = new DeleteObjectCommand({
         Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-        Key: s3Key,
+        Key: filePath,
       });
 
       await s3Client.send(deleteFileCommand);
@@ -81,13 +64,15 @@ const DeletePost: React.FC<DeletePostProps> = async ({ postId, post_by, filePath
       router.refresh();
     } catch (error) {
       console.error("Error during delete operation:", error);
+      toast.error("An error occurred while deleting the post");
     }
-
   };
 
+  if (isFetching) {
+    return null;
+  }
 
-  const userData = await getUserData();
-  if (userData?.id === post_by) {
+  if (user?.id === post_by) {
     return (
       <AlertDialog>
         <AlertDialogTrigger>
@@ -115,3 +100,136 @@ const DeletePost: React.FC<DeletePostProps> = async ({ postId, post_by, filePath
 };
 
 export default DeletePost;
+
+// // app/components/DeletePost.tsx
+// "use client";
+// import React from "react";
+// import { Button } from "../ui/button";
+// import useUser from "@/app/hook/useUser";
+// import { supabaseBrowser } from "@/lib/supabase/browser";
+// import { toast } from "sonner";
+// import { useRouter } from "next/navigation";
+// import { RiDeleteBin6Line } from "react-icons/ri";
+
+// import { MediaFile } from "@/lib/types/types";
+// import {
+//   AlertDialog,
+//   AlertDialogAction,
+//   AlertDialogCancel,
+//   AlertDialogContent,
+//   AlertDialogDescription,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogTitle,
+//   AlertDialogTrigger,
+// } from "@/components/ui/alert-dialog";
+
+// interface DeletePostProps {
+//   post_by: string;
+//   image: string;
+//   event_id?: string;
+//   team_id?: string;
+//   player_id?: string;
+// }
+
+// const DeletePost: React.FC<DeletePostProps> = ({
+//   post_by,
+//   image,
+//   event_id,
+//   team_id,
+//   player_id
+// }) => {
+//   const { data: user, isFetching } = useUser();
+//   const router = useRouter();
+
+//   const handleDelete = async () => {
+//     toast.info("Deleting image...");
+//     try {
+//       const supabase = supabaseBrowser();
+//       let folderPath = event_id
+//         ? `events/${post_by}/${event_id}/${team_id}/`
+//         : `players/${post_by}/${player_id}/`;
+//       let imagePath = image.split(folderPath).pop() ?? "";
+//       if (imagePath.startsWith("/")) {
+//         imagePath = imagePath.substring(1);
+//       }
+//       const fullPath = `${folderPath}${imagePath}`;
+//       const { data, error } = await supabase.storage
+//         .from("media")
+//         .remove([fullPath]);
+
+//         console.log("data", post_by, event_id, team_id, folderPath, imagePath, fullPath, data, error);
+//       if (error) {
+//         console.error("Failed to delete image:", error);
+//         toast.error(`Failed to delete image: ${error.message}`);
+//       } else {
+//         console.log("Successfully removed image with path:", fullPath, data);
+//         toast.success("Successfully removed image");
+//         router.refresh();
+//       }
+//     } catch (error) {
+//       console.error("Error during delete operation:", error);
+//       toast.error("An error occurred while deleting the image");
+//     }
+//   };
+  
+//   if (isFetching) {
+//     return null;
+//   }
+
+//   if (user?.id === post_by) {
+//     return (
+//       <AlertDialog>
+//         <AlertDialogTrigger>
+//           <span className="text-sm flex items-center cursor-pointer">
+//             <RiDeleteBin6Line className="text-xl mr-2" />
+//             Delete Media File
+//           </span>{" "}
+//         </AlertDialogTrigger>
+//         <AlertDialogContent>
+//           <AlertDialogHeader>
+//             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//             <AlertDialogDescription>
+//               This action cannot be undone. This will permanently delete your
+//               account and remove your data from our servers.
+//             </AlertDialogDescription>
+//           </AlertDialogHeader>
+//           <AlertDialogFooter>
+//             <AlertDialogCancel>Cancel</AlertDialogCancel>
+//             <AlertDialogAction onClick={handleDelete}>
+//               Continue
+//             </AlertDialogAction>
+//           </AlertDialogFooter>
+//         </AlertDialogContent>
+//       </AlertDialog>
+//     );
+//   } else {
+//     return (
+//       <AlertDialog>
+//         <AlertDialogTrigger>
+//           <span className="text-sm flex items-center cursor-pointer">
+//             <RiDeleteBin6Line className="text-xl" />
+//             Delete Media File
+//           </span>{" "}
+//         </AlertDialogTrigger>
+//         <AlertDialogContent>
+//           <AlertDialogHeader>
+//             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//             <AlertDialogDescription>
+//               This action cannot be undone. This will permanently delete your
+//               account and remove your data from our servers.
+//             </AlertDialogDescription>
+//           </AlertDialogHeader>
+//           <AlertDialogFooter>
+//             <AlertDialogCancel>Cancel</AlertDialogCancel>
+//             <AlertDialogAction onClick={handleDelete}>
+//               Continue
+//             </AlertDialogAction>
+//           </AlertDialogFooter>
+//         </AlertDialogContent>
+//       </AlertDialog>
+//     );
+//   }
+// };
+
+// export default DeletePost;
